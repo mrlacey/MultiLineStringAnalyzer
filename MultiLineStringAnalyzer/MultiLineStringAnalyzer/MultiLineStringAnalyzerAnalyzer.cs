f357606b966cbfd08ac2db4f6af5fe39179d6d9f
Matt Lacey <matt@mrlacey.co.uk>
2018-11-14 18:19:02 +0000

    Changed default severity to Info
    So can install in a project without raising Warnings

--- MultiLineStringAnalyzer/MultiLineStringAnalyzer/MultiLineStringAnalyzerAnalyzer.cs
+++ MultiLineStringAnalyzer/MultiLineStringAnalyzer/MultiLineStringAnalyzerAnalyzer.cs
@@ -16,7 +16,7 @@ namespace MultiLineStringAnalyzer
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Formatting";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
